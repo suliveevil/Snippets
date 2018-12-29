3152f14fb425d342a8b86d3a9190d519b662c3f7
suliveevil <1361136657@qq.com>
2018-12-29 08:47:07 +0800

    update contents

---
+++ Shell/BAB-shell/eudic-sed.sh
@@ -0,0 +1,5 @@
+set clipboard to $wordquery
+
+sed -i "s/query/$wordquery/g" eudic.sh
+
+/Users/suliveevil/allmyFiles/GitFiles/Snippets/Shell/BAB-shell/eudic.sh
---
+++ Softwares/Keyboard Maestro.md
@@ -0,0 +1,2 @@
+[manual:Application Switcher [Keyboard Maestro Wiki]](https://wiki.keyboardmaestro.com/manual/Application_Switcher)
+
