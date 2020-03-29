01e0a6f7d37c90a9d8c1d7076e44b8ec302b8486
suliveevil <1361136657@qq.com>
2020-03-29 17:13:40 +0800

    Update Python之禅.py

--- Python/Python之禅.py
+++ Python/Python之禅.py
@@ -14,7 +14,7 @@ Readability counts.
 Special cases aren't special enough to break the rules.
 Although practicality beats purity.
 Errors should never pass silently.
-Unless explicitly silenced.
+Unless explicitly silenced
 In the face of ambiguity, refuse the temptation to guess.
 There should be one-- and preferably only one --obvious way to do it.
 Although that way may not be obvious at first unless you're Dutch.
@@ -25,3 +25,7 @@ If the implementation is easy to explain, it may be a good idea.
 Namespaces are one honking great idea -- let's do more of those!
 """
 
+compile(this)
+┌a9┐
+|a9|
+└a9┘
