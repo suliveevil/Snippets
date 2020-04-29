0f12ee30b5db626c094ef0669a797e67155d6728
suliveevil_iPadPro11 <1361136657@qq.com>
2020-04-29 19:29:52 +0800

    InstantCopy update

--- 0Softwares/MarginNote/InstantCopy/main.js
+++ 0Softwares/MarginNote/InstantCopy/main.js
@@ -1,92 +1,110 @@
-JSB.newAddon = function(mainPath) {
-    var newAddonClass= JSB.defineClass('InstantCopyAddon : JSExtension', {
-        //Mark: - Instance Method Definitions
-        // Window initialize
-        sceneWillConnect: function() {
+JSB.newAddon = function (mainPath) {
+    return JSB.defineClass('InstantCopy : JSExtension', /*Instance members*/{
+        //Window initialize
+        sceneWillConnect: function () {
+            self.webController = WebViewController.new();
         },
-        // Window disconnect
-        sceneDidDisconnect: function() {
+        //Window disconnect
+        sceneDidDisconnect: function () {
         },
-        // Window resign active
-        sceneWillResignActive: function() {
+        //Window resign active
+        sceneWillResignActive: function () {
         },
-        // Window become active
-        sceneDidBecomeActive: function() {
+        //Window become active
+        sceneDidBecomeActive: function () {
         },
-        //MARK: MN behaviors
-        notebookWillOpen: function(notebookid) {
+        notebookWillOpen: function (notebookid) {
+            // New Excerpt
+            NSNotificationCenter.defaultCenter().addObserverSelectorName(self, 'onProcessExcerptText:', 'ProcessNewExcerpt');
+            // Change Excerpt
+            NSNotificationCenter.defaultCenter().addObserverSelectorName(self, 'onProcessExcerptText:', 'ChangeExcerptRange');
+            // Selecting text on pdf or epub
             NSNotificationCenter.defaultCenter().addObserverSelectorName(self, 'onPopupMenuOnSelection:', 'PopupMenuOnSelection');
+            // Clicking note
+            NSNotificationCenter.defaultCenter().addObserverSelectorName(self, 'onPopupMenuOnNote:', 'PopupMenuOnNote');
             self.instantCopy = NSUserDefaults.standardUserDefaults().objectForKey('marginnote_instantcopy');
         },
-        notebookWillClose: function(notebookid) {
-            NSNotificationCenter.defaultCenter().removeObserverName(self,'PopupMenuOnSelection');
+        notebookWillClose: function (notebookid) {
+            NSNotificationCenter.defaultCenter().removeObserverName(self, 'ProcessNewExcerpt');
+            NSNotificationCenter.defaultCenter().removeObserverName(self, 'ChangeExcerptRange');
+            NSNotificationCenter.defaultCenter().removeObserverName(self, 'PopupMenuOnSelection');
+            NSNotificationCenter.defaultCenter().removeObserverName(self, 'PopupMenuOnNote');
         },
-        documentDidOpen: function(docmd5) {
+        documentDidOpen: function (docmd5) {
         },
-        docmentWillClose: function(docmd5) {
+        documentWillClose: function (docmd5) {
         },
-        controllerWillLayoutSubviews: function(controller) {
+        controllerWillLayoutSubviews: function (controller) {
         },
-        queryAddonCommandStatus: function() {
-            if(Application.sharedInstance().studyController(self.window).studyMode < 3) {
+        queryAddonCommandStatus: function () {
+            if (Application.sharedInstance().studyController(self.window).studyMode < 3)
                 return {
-                    image: 'instantcopy.png',
+                    image: 'instantCopy.png',
                     object: self,
-                    selector: "toggleInstantCopy:",
-                    checked: (self.instantCopy?true:false)
+                    selector: 'toggleInstantCopy:',
+                    checked: self.instantCopy
                 };
-            }
             return null;
         },
-
-        // Selecting text on document and Copy to Clipboard
-        onPopupMenuOnSelection: function(sender) {
-            if(!Application.sharedInstance().checkNotifySenderInWindow(sender,self.window)) {
-                return; // Don't process message from other window
-            }
-            if(!self.instantCopy) {
-                return;
-            }
-            var text = sender.userInfo.documentController.selectionText;
-            if(text && text.length) {
+        onPopupMenuOnSelection: function (sender) {
+            if (!Application.sharedInstance().checkNotifySenderInWindow(sender, self.window)) return;//Don't process message from other window
+            if (!self.instantCopy) return;
+            let text = sender.userInfo.documentController.selectionText;
+            if (text && text.length) {
                 // Copy to Clipboard
-                var pasteBoard = UIPasteboard.generalPasteboard();
+                let pasteBoard = UIPasteboard.generalPasteboard();
                 pasteBoard.string = text;
             }
         },
-
-        // toggleInstantCopy
-        toggleInstantCopy: function(sender) {
-            var lan = NSLocale.preferredLanguages().length?NSLocale.preferredLanguages()[0].substring(0,2):'en';
-            if(self.instantCopy) {
+        onPopupMenuOnNote: function (sender) {
+            if (!Application.sharedInstance().checkNotifySenderInWindow(sender, self.window)) return;//Don't process message from other window
+            if (!self.instantCopy) return;
+            let note = sender.userInfo.note;
+            let titleText = note.noteTitle;
+            let excerptText = note.excerptText.split("**")[0];
+            let text = titleText || excerptText;
+            if (text && text.length) {
+                // Copy to Clipboard
+                let pasteBoard = UIPasteboard.generalPasteboard();
+                pasteBoard.string = text;
+            }
+        },
+        onProcessExcerptText: function (sender) {
+            if (!Application.sharedInstance().checkNotifySenderInWindow(sender, self.window)) return;//Don't process message from other window
+            if (!self.instantCopy) return;
+            let noteid = sender.userInfo.noteid;
+            let note = noteid ? Database.sharedInstance().getNoteById(noteid) : {excerptText: '', noteTitle: ''};
+            let text = note.excerptText || note.noteTitle;
+            if (text && text.length) {
+                // Copy to Clipboard
+                let pasteBoard = UIPasteboard.generalPasteboard();
+                pasteBoard.string = text;
+            }
+        },
+        toggleInstantCopy: function (sender) {
+            var lan = NSLocale.preferredLanguages().length ? NSLocale.preferredLanguages()[0].substring(0, 2) : 'en';
+            let tips = lan === 'zh' ? '自动复制已关闭' : 'InstantCopy is turned off';
+            if (self.instantCopy) {
                 self.instantCopy = false;
-                if(lan == 'zh')
-                    Application.sharedInstance().showHUD('自动复制已关闭',self.window,2);
-                else
-                    Application.sharedInstance().showHUD('InstantCopy is turned off',self.window,2);
             } else {
                 self.instantCopy = true;
-                if(lan == 'zh')
-                    Application.sharedInstance().showHUD('选中的文字会被自动复制到系统剪贴板',self.window,2);
-                else
-                    Application.sharedInstance().showHUD('The selected text will be copied to the clipboard simultaneously',self.window,2);
+                tips = lan === 'zh' ? '选中的文字会被自动复制到系统剪贴板' : 'The selected text will be copied to the clipboard simultaneously';
             }
+            Application.sharedInstance().showHUD(tips, self.window, 2);
             NSUserDefaults.standardUserDefaults().setObjectForKey(self.instantCopy, 'marginnote_instantcopy');
             Application.sharedInstance().studyController(self.window).refreshAddonCommands();
         },
-
-    }, {
-        //MARK: - Class Method Definitions
-        addonDidConnect: function() {
+    }, /*Class members*/{
+        addonDidConnect: function () {
         },
-        addonWillDisconnect: function() {
+        addonWillDisconnect: function () {
         },
-        applicationWillEnterForeground: function() {
+        applicationWillEnterForeground: function () {
         },
-        applicationDidEnterBackground: function() {
+        applicationDidEnterBackground: function () {
         },
-        applicationDidReceiveLocalNotification: function(notify) {
+        applicationDidReceiveLocalNotification: function (notify) {
         },
     });
-    return newAddonClass;
 };
+
